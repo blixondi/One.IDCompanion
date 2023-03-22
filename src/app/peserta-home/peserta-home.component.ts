fedd0577d457a5690b0dfa5e76bf68c90b909b55
blixondi <101459072+blixondi@users.noreply.github.com>
2023-03-22 12:33:03 +0700

    home peserta almost done

--- src/app/peserta-home/peserta-home.component.ts
+++ src/app/peserta-home/peserta-home.component.ts
@@ -1,14 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { PesertaService } from '../peserta.service';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-peserta-home',
   templateUrl: './peserta-home.component.html',
   styleUrls: ['./peserta-home.component.scss'],
 })
-export class PesertaHomeComponent  implements OnInit {
+export class PesertaHomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public ps: PesertaService, private storage: Storage) { }
 
-  ngOnInit() {}
+  id = 0;
+  nama_kelompok = '';
+  koin1 = '';
+  koin5 = '';
+  koin10 = '';
+  total_koin = 0;
+
+  status = ''
+
+  getDataKelompok(id: number) {
+    this.ps.getDataKelompok(id).subscribe((data) => {
+      
+      this.nama_kelompok = data['data']['0']['nama'];
+      this.koin1 = data['data']['0']['koin1'];
+      this.koin5 = data['data']['0']['koin5'];
+      this.koin10 = data['data']['0']['koin10'];
+      this.total_koin = data['data']['0']['total_koin'];
+    });
+  }
+
+  async ngOnInit() {
+    this.id = await this.storage.get('user_id');
+    this.getDataKelompok(this.id);
+  }
 
 }
--- src/app/peserta.service.ts
+++ src/app/peserta.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { HttpParams } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PesertaService {
 
-  constructor() { }
+  getDataKelompok(id: number): Observable<any> {
+    let body = new HttpParams();
+    body = body.set('id', id);
+    return this.http.post(
+      'https://noinheim.my.id/onedotid_api/getkelompokbyid.php',
+      body
+    )
+  }
+
+  getFavoriteComic(user_id: number): Observable<any> {
+    let body = new HttpParams();
+    body = body.set('id', user_id);
+    return this.http.post(
+      'https://ubaya.fun/hybrid/160420033/komik_api/getuserfavcomic.php',
+      body
+    )
+  }
+
+  constructor(private http: HttpClient) { }
 }
